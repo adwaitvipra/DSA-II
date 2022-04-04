e9641859caee4a88dd194e35af79d7cec50fa9ef
adwaitvipra <vipraadwait@gmail.com>
2022-04-04 21:43:44 +0530

    Created insertNode() and modified header file

--- A1/BST.c
+++ A1/BST.c
@@ -1,41 +1,84 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node //creating node
+typedef struct Node // creating a struct for node
 {
     int mis;
-    char name[10];
+    char *name;
     struct Node *left, *right;
 } node;
 
-typedef node *bst; //typecasting a ptr to node as bst
+typedef node *bst; // typecasting a ptr to node as bst
 
 void intiBST(bst *t)
 {
     *t = NULL;
     return;
 }
-void insertNode(bst *t, int x)
+void insertNode(bst *t, int x, char *n)
 {
+    node *nn = NULL;
+    nn = (node *)malloc(sizeof(node));
 
+    if (!nn)
+    {
+        free(nn);
+        printf("Overflow!\n");
+        return;
+    }
+
+    nn->mis = x;
+    nn->name = n;
+    nn->left = nn->right = NULL;
+
+    node *p, *q; // q is a trailing ptr of
+    p = *t;
+    q = NULL;
+
+    if (!p)
+    {
+        *t = p;
+        return;
+    }
+
+    while (!p) // move p until it becomes null
+    {
+        if (p->mis == x)
+        {
+            printf("Duplicate Entry!\n");
+            return;
+        }
+
+        q = p; // let q follow p
+        if (p->mis < x)
+            p = p->right;
+        else
+            p = p->left;
+    }
+
+    if (q->mis < x)
+        q->right = nn;
+    else
+        q->left = nn;
+    return;
 }
 int removeNode(bst *t, int key)
 {
-
+    return ;
 }
 int search(bst t, int key)
 {
-
+    return ;
 }
-void postOrder(bst t)
+void postOrder(bst t) //left right root
 {
-
+    return ;
 }
 void displayLevel(bst t, int level)
 {
-
+    return ;
 }
 void destroyTree(bst *t)
 {
-
+    return ;
 }
--- A1/BST.h
+++ A1/BST.h
@@ -1,9 +1,9 @@
 #include "BST.c"
 //This file contains all the function prototypes
 void initBST(bst*);
-void insertNode(bst*, int);
+void insertNode(bst*, int, char* );
 int removeNode(bst *, int);
 int search(bst,int);
 void postOrder(bst);
 void displayLevel(bst ,int);
-void destroyTree(bst *)
+void destroyTree(bst *);
